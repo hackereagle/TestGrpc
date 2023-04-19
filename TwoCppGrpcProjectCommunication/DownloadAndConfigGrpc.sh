c9f284e21df79d43f4c0c4b784f4e8dab0c77901
sclin <sclin@brightestTECH.com>
2023-04-19 20:12:34 +0800

    Chg: Finished download and configure grpc shell script. Chg: Modified git ignore file to ignore grpc project folder. Chg: Modified README.md to add how to use this project note.

--- TwoCppGrpcProjectCommunication/DownloadAndConfigGrpc.sh
+++ TwoCppGrpcProjectCommunication/DownloadAndConfigGrpc.sh
@@ -10,3 +10,24 @@ then
 fi
 
 # clone gRPC code
+git clone -b v1.54.0 https://github.com/grpc/grpc
+cd grpc
+echo "Current directory = $PWD"
+git submodule update --init
+
+# compiling and install
+## Use externally provided env to determine build parallelism, otherwise use default.
+GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS=${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS:-4}
+echo "Build and install gRPC with \"module\" - build dependencies alongside gRPC. The source code is obtained from gRPC's git submodules."
+mkdir cmake/build/ -p
+pushd cmake/build
+cmake ../.. -DgRPC_INSTALL=ON                \
+			-DCMAKE_BUILD_TYPE=Release       \
+			-DgRPC_ABSL_PROVIDER=module      \
+			-DgRPC_CARES_PROVIDER=module     \
+			-DgRPC_PROTOBUF_PROVIDER=module  \
+			-DgRPC_RE2_PROVIDER=module       \
+			-DgRPC_SSL_PROVIDER=module       \
+			-DgRPC_ZLIB_PROVIDER=module
+make "-j${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS}" install
+popd
--- TwoGrpcProjectCommunication/README.md
+++ TwoGrpcProjectCommunication/README.md
@@ -1,3 +1,14 @@
 # Two gRpc Project Communication
+<p>
 This is my first gRpc project. I will use MVVM testing project to modify.</br>
 I am going to abstract computing part to microservice.
+</p>
+
+Here note some point:</br>
+1. If grpc client, it must to install following packages via NuGet:
+```
+Google.protobuf 
+Grpc.Net.Client 
+Grpc.Tools
+```
+2. No matter client or server, if modified .proto file, it need to rebuild project. When we compiling, IDE will create base class and RPC stub class. We just need to inherit the base class to develop.
