7dae92ecc5437daf562dbcd402527b0a327cfe77
sclin <read0651@hotmail.com>
2023-04-25 22:27:25 +0800

    Chg: In DownloadAndConfigGrpc.sh, added code to detect os and do corresponding installation. I added Mac installation with homebrew.

--- TwoCppGrpcProjectCommunication/DownloadAndConfigGrpc.sh
+++ TwoCppGrpcProjectCommunication/DownloadAndConfigGrpc.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-echo "Would you like to install compiling tools?[y/n]"
-read NeedBuildEnv
-if [[ $NeedBuildEnv = "y" ]] || [[ $NeedBuildEnv = "Y" ]]
+if [[ "$OSTYPE" =~ ^darwin ]]
 then
-	echo "Begin install compiling tool."
-	/bin/bash ./00.ConfigBuildEnv.sh
-	echo "End install compiling tools."
+	echo "This is Mac OS!"
+	read -p "Please ensure already installed brew!" TMP
+
+	brew install grpc
+elif [[ "$OSTYPE" =~ ^linux ]]
+then
+	echo "Would you like to install compiling tools?[y/n]"
+	read NeedBuildEnv
+	if [[ $NeedBuildEnv = "y" ]] || [[ $NeedBuildEnv = "Y" ]]
+	then
+		echo "Begin install compiling tool."
+		/bin/bash ./00.ConfigBuildEnv.sh
+		echo "End install compiling tools."
+	fi
+
+	# clone gRPC code
+	git clone -b v1.54.0 https://github.com/grpc/grpc
+	cd grpc
+	echo "Current directory = $PWD"
+	git submodule update --init
+
+	# compiling and install
+	## Use externally provided env to determine build parallelism, otherwise use default.
+	GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS=${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS:-4}
+	echo "Build and install gRPC with \"module\" - build dependencies alongside gRPC. The source code is obtained from gRPC's git submodules."
+	mkdir cmake/build/ -p
+	pushd cmake/build
+	cmake ../.. -DgRPC_INSTALL=ON                \
+				-DCMAKE_BUILD_TYPE=Release       \
+				-DgRPC_ABSL_PROVIDER=module      \
+				-DgRPC_CARES_PROVIDER=module     \
+				-DgRPC_PROTOBUF_PROVIDER=module  \
+				-DgRPC_RE2_PROVIDER=module       \
+				-DgRPC_SSL_PROVIDER=module       \
+				-DgRPC_ZLIB_PROVIDER=module
+	make "-j${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS}" install
+	popd
 fi
-
-# clone gRPC code
-git clone -b v1.54.0 https://github.com/grpc/grpc
-cd grpc
-echo "Current directory = $PWD"
-git submodule update --init
-
-# compiling and install
-## Use externally provided env to determine build parallelism, otherwise use default.
-GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS=${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS:-4}
-echo "Build and install gRPC with \"module\" - build dependencies alongside gRPC. The source code is obtained from gRPC's git submodules."
-mkdir cmake/build/ -p
-pushd cmake/build
-cmake ../.. -DgRPC_INSTALL=ON                \
-			-DCMAKE_BUILD_TYPE=Release       \
-			-DgRPC_ABSL_PROVIDER=module      \
-			-DgRPC_CARES_PROVIDER=module     \
-			-DgRPC_PROTOBUF_PROVIDER=module  \
-			-DgRPC_RE2_PROVIDER=module       \
-			-DgRPC_SSL_PROVIDER=module       \
-			-DgRPC_ZLIB_PROVIDER=module
-make "-j${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS}" install
-popd
